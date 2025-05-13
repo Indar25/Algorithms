bf96909009caffce95bc930c61ec2290aacfaa21
Indar Rajput <Indar.Rajput@grapecity.com>
2025-05-13 23:38:29 +0530

    53. Maximum Subarray

---
+++ DSA-Arrays/SubarrayProblem.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA_Arrays;
+internal class SubarrayProblem
+{
+    /*
+     * https://leetcode.com/problems/maximum-subarray/description/
+     * 53. Maximum Subarray
+     */
+    public int MaxSubArray(int[] nums)
+    {
+        int maxSum = nums[0];
+
+        if (nums.Length == 1)
+        {
+            return maxSum;
+        }
+        int tempSum = maxSum;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            tempSum = nums[i] + Math.Max(tempSum, 0);
+            maxSum = Math.Max(maxSum, tempSum);
+        }
+        return maxSum;
+    }
+}
+
